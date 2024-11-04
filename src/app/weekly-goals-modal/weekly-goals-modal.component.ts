121a5112d61e547baf48a1024d0137dd19b13616
sarasriv2 <ssriva16@ucsc.edu>
2024-11-03 23:54:05 -0800

    Add files via upload

---
+++ src/app/weekly-goals-modal/weekly-goals-modal.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-weekly-goals-modal',
+  standalone: true,
+  imports: [],
+  templateUrl: './weekly-goals-modal.component.html',
+  styleUrl: './weekly-goals-modal.component.scss'
+})
+export class WeeklyGoalsModalComponent {
+
+}
---
+++ src/app/weekly-goals/weekly-goals.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-weekly-goals',
+  standalone: true,
+  imports: [],
+  templateUrl: './weekly-goals.component.html',
+  styleUrl: './weekly-goals.component.scss'
+})
+export class WeeklyGoalsComponent {
+
+}
